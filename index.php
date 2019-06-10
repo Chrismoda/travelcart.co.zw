23d2b0f03b7c57f5e8641146cce54907915fe438
Christopher <christophernyandoros@gmail.com>
2019-06-10 16:09:58 +0200

    Notifications on emails working now. Calcculations of remaining days has only one contraint as of yet

--- index.php
+++ index.php
@@ -12,16 +12,127 @@ include "includes/header.php";
 if(func::checkLoginState($dbh)){
 	//user is logged in, now search for unpaid up bookings and calculate days,
 	$us = $_SESSION['username'];
-	$uql = "SELECT * FROM `orders` WHERE `user_name`='$us' AND `date` < now()";
+	$uql = "SELECT * FROM `orders` WHERE `user_name`='$us' AND `date` > now()";
 	$uqqry = mysqli_query($dbc, $uql);
 	if (mysqli_num_rows($uqqry) != 0) {
 		#check status
 		while ($rs = mysqli_fetch_assoc($uqqry)) {
 			$dat = $rs['date'];
+			$bus = $rs['bus_id'];
+
+			$bsql = "";
+			$bqry = mysqli_query($dbc, $bsql);
+			$brs = mysqli_fetch_assoc($bqry);
+			$route = $brs['post_title'];
 			if ($rs['paid'] == 0) {
-				# code...
+				$date1=date_create($dat);
+				$date2=date_create(date('Y-m-d'));
+				$diff=date_diff($date1,$date2);
+				$actuaDF = ltrim($diff->format("%R%a"),'-');
+
+				$adf = $actuaDF;
+				//send email with notification
+				
+				$data = json_encode(array(
+						"category"=>"3",
+						"email"=> $us,
+						"message"=>"Your bus booking for route ".$route." has been left with ".$adf." days for payment. Please proceed to payment before deadline day of payment passes in ".$adf." days. Failure will result in booking being added to day of travel available seats for on site bus booking. "
+				));
+		
+		
+				$curl = curl_init();
+		
+				curl_setopt_array($curl, array(
+					CURLOPT_URL => "http://mail.teamvelocity.co.zw",
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_ENCODING => "",
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 30,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => "POST",
+					CURLOPT_POSTFIELDS => $data,
+					CURLOPT_HTTPHEADER => array(
+						"Content-Type: application/json",
+						"cache-control: no-cache"
+					),
+				));
+		
+				$response = curl_exec($curl);
+				$err = curl_error($curl);
+		
+				curl_close($curl);
+		
+				if ($err) {
+					
+				} else {
+					$rs = json_decode($response,true);
+					if($rs['status'] == "Sent"){
+						//show notification on bar
+					}else{
+						
+					}
+					
+					
+				}
 			}elseif ($rs['paid'] == 1) {
-				# code...
+
+				$date1=date_create($dat);
+				$date2=date_create(date('Y-m-d'));
+				$diff=date_diff($date1,$date2);
+				$actuaDF = ltrim($diff->format("%R%a"),'-');
+				$adf = $actuaDF;
+				//send email with notification on closing on travel Date
+				if ($actuaDF <= 2) {
+					$data = json_encode(array(
+							"category"=>"4",
+							"email"=> $us,
+							"message"=>"Your bus booking for route ".$route." has been left with ".$adf." days for travel. Please remember to update your calender and please note that you can no longer postpone travel to a later date for the minimum of 2 days notice has elapsed"
+					));
+				}else{
+					$data = json_encode(array(
+							"category"=>"4",
+							"email"=> $us,
+							"message"=>"Your bus booking for route ".$route." has been left with ".$adf." days for travel. Please remember to update your calender or postpone travel to a later before 2 days of travel"
+					));
+				}
+				
+		
+		
+				$curl = curl_init();
+		
+				curl_setopt_array($curl, array(
+					CURLOPT_URL => "http://mail.teamvelocity.co.zw",
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_ENCODING => "",
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 30,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => "POST",
+					CURLOPT_POSTFIELDS => $data,
+					CURLOPT_HTTPHEADER => array(
+						"Content-Type: application/json",
+						"cache-control: no-cache"
+					),
+				));
+		
+				$response = curl_exec($curl);
+				$err = curl_error($curl);
+		
+				curl_close($curl);
+		
+				if ($err) {
+					
+				} else {
+					$rs = json_decode($response,true);
+					if($rs['status'] == "Sent"){
+						//show notification on bar
+					}else{
+						
+					}
+					
+					
+				}
+
 			}
 		}
 
